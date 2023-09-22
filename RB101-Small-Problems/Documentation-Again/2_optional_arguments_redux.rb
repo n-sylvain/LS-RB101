a19e4fe00280caf33ee426085af3a2a5388fc127
Sylvain Ni <sylvainni@msn.com>
2023-09-22 17:21:47 +0200

    add documentation notes

---
+++ RB101-Small-Problems/Documentation-Again/2_optional_arguments_redux.rb
@@ -0,0 +1,7 @@
+require 'date'
+
+puts Date.civil               # prints "-4712-01-01"
+puts Date.civil(2016)         # prints "2016-01-01"
+puts Date.civil(2016, 5)      # prints "2016-05-01"
+puts Date.civil(2016, 5, 13)  # prints "2016-05-13"
+puts Date.civil(2016, 5, 13, Date::JULIAN)
